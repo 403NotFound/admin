d85a466882dfd6c8f6df8a811b8c1dda5b77f330
zhanglei <zhanglei@zhangleideMacBook-Pro.local>
2021-04-16 18:00:54 +0800

    feat: 新增一些 alias

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
 
-const path = require('path');
-
-function resolve(dir) {
-  return path.join(__dirname, dir);
-}
+const path = require('path')
 
 module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        'assets': path.resolve(__dirname, 'src/assets'),
+        'layout': path.resolve(__dirname, 'src/layout'),
+        'utils': path.resolve(__dirname, 'src/utils'),
+        'views': path.resolve(__dirname, 'src/views')
+      }
+    }
+  },
   css: {
     loaderOptions: {
       sass: {
